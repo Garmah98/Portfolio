a04f9b02297be64ab5429f3e95c1515dd685bc2f
Garmah <PiotrekG000@gmail.com>
2025-01-07 17:30:46 +0100

    Added basic navigation (WIP)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,31 +1,33 @@
 import { NavLink } from 'react-router'
 export default function Navigation() {
     return (
-        <ul className="flex">
-            <NavLink
-                to={'/'}
-                className={({ isActive }) =>
-                    isActive ? 'text-red-400' : undefined
-                }
-            >
-                About me
-            </NavLink>
-            <NavLink
-                to={'/technology'}
-                className={({ isActive }) =>
-                    isActive ? 'text-red-400' : undefined
-                }
-            >
-                Technology
-            </NavLink>
-            <NavLink
-                to={'/projects'}
-                className={({ isActive }) =>
-                    isActive ? 'text-red-400' : undefined
-                }
-            >
-                Projects
-            </NavLink>
-        </ul>
+        <nav className="absolute right-24 top-12 text-red-600">
+            <ul className="flex">
+                <NavLink
+                    to={'/'}
+                    className={({ isActive }) =>
+                        isActive ? 'text-red-400' : undefined
+                    }
+                >
+                    About me
+                </NavLink>
+                <NavLink
+                    to={'/technology'}
+                    className={({ isActive }) =>
+                        isActive ? 'text-red-400' : undefined
+                    }
+                >
+                    Technology
+                </NavLink>
+                <NavLink
+                    to={'/projects'}
+                    className={({ isActive }) =>
+                        isActive ? 'text-red-400' : undefined
+                    }
+                >
+                    Projects
+                </NavLink>
+            </ul>
+        </nav>
     )
 }
--- src/routes/RootLayout.tsx
+++ src/routes/RootLayout.tsx
@@ -3,9 +3,9 @@ import Navigation from '../components/Navigation'
 export default function RootLayout() {
     return (
         <>
-            <nav className="absolute right-24 top-12 text-red-600">
+           
                 <Navigation />
-            </nav>
+          
             <main>
                 <Outlet />
             </main>
