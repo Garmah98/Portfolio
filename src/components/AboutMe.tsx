54fa26ec90af527d09b093a54137436640ed528c
Garmah <PiotrekG000@gmail.com>
2025-01-07 17:30:07 +0100

    Added basic homepage (WIP)

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,12 @@
+export default function AboutMe() {
+    return (
+        <section className="p-6 bg-[#333]">
+            <h2 className="text-3xl">About me</h2>
+            <p>
+                Hi, my name is Piotr and I'm frontend developer based in Poland.
+                My journey with frontend development began about 2 years ago and
+                I've been coding ever since.
+            </p>
+        </section>
+    )
+}
--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,17 +1,12 @@
+import AboutMe from '../components/AboutMe'
 export default function HomePage() {
     return (
-        <div className="flex h-screen w-full">
-            <div className="m-auto size-11/12 bg-black text-white">
-                <h1 className="p-4 text-6xl">Piotr Gawek</h1>
-                <section className="p-6">
-                    <h2 className="text-3xl">About me</h2>
-                    <p>
-                        Hi, my name is Piotr and I'm frontend developer based in
-                        Poland. My journey with frontend development began about
-                        2 years ago and I've been coding ever since.
-                    </p>
-                </section>
+        <div className="h-screen w-full text-white">
+            <div className="flex flex-col">
+                <h1 className="p-4 text-9xl">Piotr Gawek</h1>
+                <h2 className="ml-12 text-3xl">Frontend Developer</h2>
             </div>
+            <AboutMe />
         </div>
     )
 }
