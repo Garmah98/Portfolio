34b839f30604a42816873e85995cb18a5abe2796
Garmah <PiotrekG000@gmail.com>
2025-01-08 14:50:42 +0100

    Finished basic About me page

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,11 +1,13 @@
 export default function AboutMe() {
     return (
-        <section className="p-6 bg-[#333]">
-            <h2 className="text-3xl">About me</h2>
-            <p>
+        <section className="m-auto flex h-1/3 w-2/3 flex-col items-center justify-center p-6 text-center shadow-lg">
+            <h2 className="mb-4 text-3xl font-semibold">About me</h2>
+            <p className="w-2/3">
                 Hi, my name is Piotr and I'm frontend developer based in Poland.
                 My journey with frontend development began about 2 years ago and
-                I've been coding ever since.
+                I've been self-learning ever since. I'm seeking new experiences
+                and oopportunity to broaden my horizons as{' '}
+                <span className="font-semibold">Junior Frontend Developer.</span>
             </p>
         </section>
     )
