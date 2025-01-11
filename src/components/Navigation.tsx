fc4cc205c955baaeda8a8415f650268fe422442d
Garmah <PiotrekG000@gmail.com>
2025-01-11 12:12:40 +0100

    Fixed navigation z-index

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,8 @@
 import { NavLink } from 'react-router'
 export default function Navigation() {
     return (
-        <nav className="absolute right-20 top-8  text-red-600">
-            <ul className="flex gap-4 text-white ">
+        <nav className="absolute right-20 top-8 z-10 text-red-600">
+            <ul className="flex gap-4 text-white">
                 <NavLink
                     to={'/'}
                     className={({ isActive }) =>
