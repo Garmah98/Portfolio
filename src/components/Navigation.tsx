40cec568e320fcd3ae054eb04380859e95e9fb72
Garmah <PiotrekG000@gmail.com>
2025-01-11 17:13:13 +0100

    Changed navigation active color

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,12 @@
 import { NavLink } from 'react-router'
 export default function Navigation() {
     return (
-        <nav className="absolute right-20 top-8 z-10 text-red-600">
+        <nav className="absolute right-20 top-8 z-10">
             <ul className="flex gap-4 text-white">
                 <NavLink
                     to={'/'}
                     className={({ isActive }) =>
-                        isActive ? 'text-red-400' : undefined
+                        isActive ? 'text-green-700' : undefined
                     }
                 >
                     About me
@@ -14,7 +14,7 @@ export default function Navigation() {
                 <NavLink
                     to={'/technology'}
                     className={({ isActive }) =>
-                        isActive ? 'text-red-400' : undefined
+                        isActive ? 'text-green-700' : undefined
                     }
                 >
                     Technology
@@ -22,7 +22,7 @@ export default function Navigation() {
                 <NavLink
                     to={'/projects'}
                     className={({ isActive }) =>
-                        isActive ? 'text-red-400' : undefined
+                        isActive ? 'text-green-700' : undefined
                     }
                 >
                     Projects
