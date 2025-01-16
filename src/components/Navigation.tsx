12469ec3caa0b052afc8bef0cc84a0ecb56b6fc8
Garmah <PiotrekG000@gmail.com>
2025-01-16 18:08:32 +0100

    Added translation to navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from 'react-router'
 import SelectButton from './ui/SelectButton'
+import { useTranslation } from 'react-i18next'
 export default function Navigation() {
+    const { t } = useTranslation()
     return (
         <nav className="absolute right-20 top-8 z-10">
             <ul className="flex items-center gap-4 text-white">
@@ -10,7 +12,7 @@ export default function Navigation() {
                         isActive ? 'text-green-700' : undefined
                     }
                 >
-                    About me
+                    {t('AboutMe.title')}
                 </NavLink>
                 <NavLink
                     to={'/technology'}
@@ -18,7 +20,7 @@ export default function Navigation() {
                         isActive ? 'text-green-700' : undefined
                     }
                 >
-                    Technology
+                    {t('Technology.title')}
                 </NavLink>
                 <NavLink
                     to={'/projects'}
@@ -26,7 +28,7 @@ export default function Navigation() {
                         isActive ? 'text-green-700' : undefined
                     }
                 >
-                    Projects
+                    {t('Projects.title')}
                 </NavLink>
                 <SelectButton />
             </ul>
