73a1313f2dbf63d360a7a36df725997e6383ef19
Garmah <PiotrekG000@gmail.com>
2025-02-19 14:31:45 +0100

    Fixed nav links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,7 @@ export default function Navigation() {
     const { t } = useTranslation()
     return (
         // flex items-center gap-4 text-white
-        <nav className="z-10 xl:absolute xl:right-8 xl:top-8">
+        <nav id='mainNav' className="z-10 xl:absolute xl:right-8 xl:top-8">
             <ul className="mb-4 flex w-screen items-center justify-center text-center text-white max-xl:shadow-lg sm:gap-4 xl:mb-0 xl:flex xl:w-auto xl:items-center">
                 <NavLink
                     to={'/'}
