06e62a547c4a0a1c4660953ceb3274a233573588
Garmah <PiotrekG000@gmail.com>
2025-04-05 15:27:46 +0200

    Adjusted lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router'
 import RootLayout from './routes/RootLayout'
 import ErrorPage from './routes/ErrorPage'
 import i18next from 'i18next'
-
+import Technology from './routes/Technology'
 
 const HomePage = lazy(() => import('./routes/HomePage'))
-const Technology = lazy(() => import('./routes/Technology'))
 const Projects = lazy(() => import('./routes/Projects'))
 
+export const fallback = (
+    // absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl
+    <div className="w-full h-full flex justify-center items-center text-2xl">
+        Loading...
+    </div>
+)
 function App() {
     const selectedLanguage = localStorage.getItem('selectedLng')
     i18next.changeLanguage(selectedLanguage!)
@@ -22,7 +27,7 @@ function App() {
                     index: true,
                     path: '',
                     element: (
-                        <Suspense fallback={<div>Loading...</div>}>
+                        <Suspense fallback={fallback}>
                             <HomePage />
                         </Suspense>
                     ),
@@ -30,15 +35,15 @@ function App() {
                 {
                     path: 'technology',
                     element: (
-                        <Suspense fallback={<div>Loading...</div>}>
+                        
                             <Technology />
-                        </Suspense>
+                      
                     ),
                 },
                 {
                     path: 'projects',
                     element: (
-                        <Suspense fallback={<div>Loading...</div>}>
+                        <Suspense fallback={fallback}>
                             <Projects />
                         </Suspense>
                     ),
--- src/routes/Technology.tsx
+++ src/routes/Technology.tsx
@@ -1,6 +1,11 @@
 import { useTranslation } from 'react-i18next'
 import ListItem from '@/components/ListItem'
-import SkillList from '@/components/SkillList'
+// import SkillList from '@/components/SkillList'
+import { lazy, Suspense } from 'react'
+import { fallback } from '@/App'
+
+const SkillList = lazy(() => import('@/components/SkillList'))
+
 export default function Technology() {
     const { t } = useTranslation()
     return (
@@ -8,25 +13,29 @@ export default function Technology() {
             <h2 className="mt-4 p-2 font-Title text-3xl lg:text-4xl">
                 {t('Technology.currentSkills')}
             </h2>
-            <SkillList>
-                <ListItem title="Html5" />
-                <ListItem title="Css" />
-                <ListItem title="Sass" />
-                <ListItem title="Tailwindcss" />
-                <ListItem title="Javascript" />
-                <ListItem title="Typescript" />
-                <ListItem title="React" />
-                <ListItem title="Redux" />
-                <ListItem title="React Router" />
-            </SkillList>
+            <Suspense fallback={fallback}>
+                <SkillList>
+                    <ListItem title="Html5" />
+                    <ListItem title="Css" />
+                    <ListItem title="Sass" />
+                    <ListItem title="Tailwindcss" />
+                    <ListItem title="Javascript" />
+                    <ListItem title="Typescript" />
+                    <ListItem title="React" />
+                    <ListItem title="Redux" />
+                    <ListItem title="React Router" />
+                </SkillList>
+            </Suspense>
 
             <h3 className="mt-4 font-Title text-2xl lg:text-3xl">
                 {t('Technology.expandList')}
             </h3>
-            <SkillList>
-                <ListItem title="NodeJs" />
-                <ListItem title="NextJs" />
-            </SkillList>
+            <Suspense fallback={fallback}>
+                <SkillList>
+                    <ListItem title="NodeJs" />
+                    <ListItem title="NextJs" />
+                </SkillList>
+            </Suspense>
         </section>
     )
 }
